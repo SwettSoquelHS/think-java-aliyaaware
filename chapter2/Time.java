40075a0d12754fe1f4ff95d753ec8d13620f50a8
aliyaware <aware06@sccs-stu.net>
2018-08-30 11:36:45 -0700

    new time

---
+++ chapter2/Time.java
@@ -0,0 +1,12 @@
+public class Time{
+	public static void main(String[] args){
+		int hour = 14;
+		int minute = 60;
+		int second = 60;
+		
+
+		int answer = hour*minute*second;
+
+		System.out.println("It has been "answer+ " seconds since midnight");
+	}
+}
