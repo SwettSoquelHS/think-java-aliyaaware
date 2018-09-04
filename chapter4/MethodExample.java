685355f7287960ca72e7167fef54254dc26111bd
aliyaware <aware06@sccs-stu.net>
2018-09-04 11:07:11 -0700

    chapter3

---
+++ chapter4/MethodExample.java
@@ -0,0 +1,11 @@
+public class MethodExample{
+	public static void main(String[] args){
+		System.out.print("hello");
+	}
+// prints a new line
+	
+	public statuc void newLine(){
+		System.ou.println("");
+
+	} // End of newLine
+}
--- exercise2.java
+++ exercise2.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 
 public class Exercise1{
 	public static void main(String[] args){
-		int in = 3;
-		System.out.printf("hello %s, in %d\n")
+		
+		// prompt user input
+		System.out.print("Enter a temperature in Celcius: ")
+		celcius = input.nextDouble();
+
+		// math for conversion
+		farenheit = celcius*(9/5)+32;
+
+		//print resulr
+		System.out.printd(farenheit%d + " F");
+
 
 	}
 }
