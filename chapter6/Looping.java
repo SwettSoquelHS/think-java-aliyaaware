ff795252e8bf79133ecb00b4a7f5eaf36c742cc2
aliyaaware <42653553+aliyaaware@users.noreply.github.com>
2018-09-11 11:10:27 -0700

    Add files via upload

---
+++ chapter6/Looping.java
@@ -0,0 +1,17 @@
+public class Looping {
+	public static void main (String[] args){
+		System.out.println("Loop master!");
+
+		int i = 0
+		while (i< 10){
+			i ==10;
+			print ("I is: " + i);
+
+		}
+	}
+
+	public static void print(String s){
+
+		System.out.println(s);
+	}
+}
