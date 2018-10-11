4428a2193a7606940fd7e86852077a846aaaecfa
aliyaaware <aliyaware1020@gmail.com>
2018-10-11 12:04:34 -0700

    exercise3

--- chapter7/exercise3.java
+++ chapter7/exercise3.java
@@ -1,20 +1,17 @@
 public class exercise3{
 
 	public static void main(String[] args){
-		int[] largeArray = new int[4];
-		int min =0;
-		int max = largeArray;
-		largeArray[0] = 1;
-		largeArray[1]= 2;
-		largeArray[2]=3;
-		largeArray[3]=4;
+		int largeArray[] = new int[]{1, 3, 4, 56, 5};
+		int max=largeArray[0];
+		int index =0;
 		for (int i=0; i<largeArray.length; i++){
-			if (largeArray[i]>= min){
-				i=max;
-			}
-			
-			System.out.println("arrays = " + largeArray[i] + max);
+			if (max< largeArray[i]){
+					max= largeArray[i];
+					index = i;
+				}					
 		}
+
+			System.out.println("Index position of the Maximum value of the array is:  " + index);
 	}
 }
 
@@ -23,5 +20,8 @@ public class exercise3{
 
 /*Write a method called indexOfMax that takes an array of integers and returns 
 	the index of the largest element. Can you write this method using an enhanced
-	for loop? Why or why not?*/
+	for loop? Why or why not?
+
+
+	No you cannot use an enhanced for loop because it asks for specific index value*/
 	
