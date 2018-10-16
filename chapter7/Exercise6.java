1329149e146f8f15a0aab731692d3c0c388098cb
aliyaware <aware06@sccs-stu.net>
2018-10-16 12:20:09 -0700

    exercises

--- chapter7/Exercise6.java
+++ chapter7/Exercise6.java
@@ -1,15 +1,15 @@
-public class Exercise5{
+public class Exercise6{
 
     public static void main(String[] args){
         int n =56;
         System.out.print(areFactors(n)
 
     }
-    public static int areFactors(int n, int m){
+    public static int arePrimeFactors(int n, int m){
         array = new int[]{1, 2, 3, 44, 34, 23, 26}
     }
 }
 
-/*Write a method named areFactors that takes an integer n 
+/*Write a method named arePrimeFactors that takes an integer n 
 and an array of integers, and that returns true if the numbers 
-in the array are all factors of n (which is to say that n is divisible by all of them).*/
+in the array are all prime and their product is n.*/
---
+++ chapter7/Exercise8.java
@@ -0,0 +1,14 @@
+public class Exercise8{
+
+    public static void main(String[] args){
+
+
+    }
+    public static int anagram(int n, int m){
+        array = new int[]{1, 2, 3, 44, 34, 23, 26}
+    }
+}
+
+/*Two words are anagrams if they contain the same letters and the same number of each letter. 
+For example, “stop” is an anagram of “pots” and “allen downey” is an anagram of “well annoyed”. 
+Write a method that takes two strings and checks whether they are anagrams of each other.*/
