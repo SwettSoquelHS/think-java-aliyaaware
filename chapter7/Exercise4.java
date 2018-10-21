46d434bd5ca3e7a340c35889465cb70978855caa
aliyaaware <aliyaware1020@gmail.com>
2018-10-21 13:21:16 -0700

    ex4

--- chapter7/Exercise4.java
+++ chapter7/Exercise4.java
@@ -1,15 +1,15 @@
 public class Exercise4{
     public static void main(String[] args){
-        int n = 3;
-        System.out.println("The number is prime? " + sieve(n));
+        int n = 1234;
+        System.out.println(sieve(n));
     }
-    public static boolean sieve(int n){
+    public static boolean sieve(int x){
 
-        while (n>=1)
+        while (x>0)
         {
-            for (int i = 2; i <= n-1; i++) 
+            for (int i = 2; i <= x-1; i++) 
             {
-                if ( n%i ==0)
+                if ( x%i ==0)
                     return false;
             }
             return true;
@@ -17,9 +17,3 @@ public class Exercise4{
         return true;
     }
 }
-
-
-
-
-// Write a method called sieve that takes an integer parameter, n, and returns a 
-//boolean array that indicates, for each number from 0 to n - 1, whether the number is prime.
