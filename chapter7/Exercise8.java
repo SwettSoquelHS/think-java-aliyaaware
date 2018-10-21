f3aad87c9e51d51af45f949a0531a6f4359ca635
aliyaaware <aliyaware1020@gmail.com>
2018-10-21 13:14:32 -0700

    homework finish

--- chapter7/Exercise8.java
+++ chapter7/Exercise8.java
@@ -1,14 +1,31 @@
+import java.util.Arrays;
 public class Exercise8{
 
     public static void main(String[] args){
-
-
+    	String a = "well done";
+    	String b = "dole lewn";
+    	System.out.print(isAnagram(a,b));
     }
-    public static int anagram(int n, int m){
-        array = new int[]{1, 2, 3, 44, 34, 23, 26}
+    public static boolean isAnagram(String a, String b){
+    	//compare the string lengths
+    	int n1 = a.length(); 
+        int n2 = b.length();
+        if (n1 != n2) 
+            return false;
+    
+
+    char[] chars = a.toCharArray();
+       
+        for(char c : chars){
+            int index = b.indexOf(c);
+            if(index != -1){
+                b = b.substring(0,index) + b.substring(index +1, b.length());
+            }else{
+                return false;
+            }           
+        }
+       
+        return true;
     }
+
 }
-
-/*Two words are anagrams if they contain the same letters and the same number of each letter. 
-For example, “stop” is an anagram of “pots” and “allen downey” is an anagram of “well annoyed”. 
-Write a method that takes two strings and checks whether they are anagrams of each other.*/
