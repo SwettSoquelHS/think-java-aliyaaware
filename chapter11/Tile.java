69811a0599fa256dd1cc69435cd7495dfad0d73d
aliyaware <aware06@sccs-stu.net>
2018-11-01 12:18:42 -0700

    work

--- chapter11/Tile.java
+++ chapter11/Tile.java
@@ -2,34 +2,44 @@ public class Tile{
 	
 
 	public static void main (String[] args){
-		int value = 7;
+		int value = 1;
 		char letter = 'a';
 		Tile t  = new Tile(value, letter);
-		System.out.println("Test: " + t.letter + t.value);
-	}
 
-	class Tile(char letter, int value){
+
+		printTile(letter);
+	}
+	char letter;
+	int value;
+
+	public Tile(char letter, int value){
 		this.letter= letter;
 		this.value = value;
 	}
 
-	class printTile(){
+	static void printTile(Tile t){
+		System.out.println(t.letter + " - "+ t.value);
+	}
+
+	static void testTile(Tile t){
+
+	}
+
+	public String toString(Tile m){
 		System.out.println("letter = " + letter);
-		System.out.println("value o it  " + value);
+		System.out.println("value o it " + value);
 	}
-	void setLetter(char character){
+	// void setLetter(char character){
 
-	}
-	char getLetter(){
+	// }
+	// char getLetter(){
 
-	}
-	void setValue(int value){
+	// }
+	// void setValue(int value){
 
-	}
-	int getValue(){
-		
-	}
+	// }
+	// int getValue(){
 
-	char letter;
-	int value;
+	// }
+
 }
