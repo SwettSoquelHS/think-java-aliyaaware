a3cf3c9c97a1bccd7cbfa34d47b3e8b5f3642dca
aliyaaware <aliyaware1020@gmail.com>
2018-11-04 12:42:58 -0800

    tile

--- chapter11/Tile.java
+++ chapter11/Tile.java
@@ -2,34 +2,51 @@ public class Tile{
 	
 
 	public static void main (String[] args){
-		int value = 7;
-		char letter = 'a';
-		Tile t  = new Tile(value, letter);
-		System.out.println("Test: " + t.letter + t.value);
+		//int value = 7;
+		//char letter = 'a';
+		Tile t2 = testTile();
+		t2.setValue(20);
+		printTile(t2);		
+		System.out.println(t2.equals(testTile()) +" "+t2.getLetter());
 	}
+	private char letter;
+	private int value;
 
-	class Tile(char letter, int value){
+	public Tile(char letter, int value){
 		this.letter= letter;
 		this.value = value;
 	}
 
-	class printTile(){
-		System.out.println("letter = " + letter);
-		System.out.println("value o it  " + value);
+	public static void printTile(Tile t){
+		System.out.println("letter = " + t.letter + "value of it  " + t.value);
 	}
-	void setLetter(char character){
+	public static Tile testTile(){
+		Tile t  = new Tile('z', 10);
+		printTile(t);
+		return t;
+	}
+	public String toString(){
+      return "A tile with "+this.letter+" and "+this.value;
+   }
+   public boolean equals(Tile that){
+      return this.letter == that.letter && this.value == that.value;
+   }
+
+	public void setLetter(char letter){
+		this.letter = letter;
 
 	}
-	char getLetter(){
+	public char getLetter(){
+		return this.letter;
 
 	}
-	void setValue(int value){
+	public void setValue(int value){
+		this.value = value;
 
 	}
-	int getValue(){
+	public int getValue(){
+		return this.value;
 		
 	}
 
-	char letter;
-	int value;
 }
