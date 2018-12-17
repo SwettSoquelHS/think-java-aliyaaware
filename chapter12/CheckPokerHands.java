f901aff64c4f3c106ece9c24424c90df58d84760
aliyaaware <aliyaware1020@gmail.com>
2018-12-16 17:16:54 -0800

    idk

--- chapter12/CheckPokerHands.java
+++ chapter12/CheckPokerHands.java
@@ -20,54 +20,22 @@ public class CheckPokerHands {
         CheckPokerHands hand = new CheckPokerHands();
         System.out.println("is it a flush? " + hand.getRank());
 
-        Card[] straight = hand.isStraight();
+        Card[] straight = theDeck.dealHand(5);
         for (Card x: straight){
          System.out.println("the card is "+ x.toString());
         }
-
-        // if (straight == x)
-        //     System.out.println("help " + x);
-        
-    }
-
-    // which accepts an array of cards as its only parameter
-
-public class CheckPokerHands {
-
-    public static void main(String[] args){
-        Card clubs8 = new Card(8, Card.CLUBS );
-        System.out.println("the card in my hand is: " + clubs8);
-
-
-        Card heartsQueen = new Card (12, Card.HEARTS);
-        System.out.println("Second card in my hand is: " + heartsQueen);
-
-
-
-     //and determines whether or 
-    //   not the array of Cards represents a Royal Flush
-
-        Deck theDeck = new Deck();
-        System.out.println("the top card is " + theDeck.topCard());
-        theDeck.shuffle();
-        System.out.println("the top card is " + theDeck.topCard());
-
-        Card[] aHand = theDeck.dealHand(5);
-        for (Card c : aHand){
-            System.out.println("the card is "+ c.toString());
-        }
-
-        CheckPokerHands hand = new CheckPokerHands();
-        System.out.println("is it a flush? " + hand.getRank());
+        System.out.println(hand.toString());
 
         // Card[] straight = hand.isStraight();
         // for (Card c: straight){
         //  System.out.println("the card is "+ straight);
-        // }
+        //}
 
-
+        //if (straight == x)
+          //  System.out.println("help " + x);
         
     }
+
 
     public static boolean isRoyalFlush(Card[] hand){
       
