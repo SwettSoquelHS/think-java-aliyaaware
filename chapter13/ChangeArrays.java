fff653995b6c06488f380580af54a2c185c215fa
aliyaaware <aliyaware1020@gmail.com>
2019-02-03 19:06:46 -0800

    done

--- chapter13/ChangeArrays.java
+++ chapter13/ChangeArrays.java
@@ -44,71 +44,75 @@ public class ChangeArrays {
         int[] arr2 = makeCopyOf(scores);
         //System.out.println("Copy of scores looks like: " + scores);
 
+        int[] first_arr = new int[]{5, 6, 7 ,8};
+        System.out.println("origional array = " + Arrays.toString(first_arr));
 
-        fin = makeCopyOf(scores);
+        
+        fin = makeCopyOf(first_arr);
         System.out.println("copy of full array " + Arrays.toString(fin));
 
-        fin = addValue(scores, 2);
+        first_arr =  new int[]{5, 6, 7 ,8};
+
+        fin = addValue(first_arr, -9, 3);
         System.out.println("add one value to array " + Arrays.toString(fin));
 
-        fin = removeLast(scores);
+        first_arr =  new int[]{5, 6, 7 ,8};
+
+        fin = removeLast(first_arr);
         System.out.println("Remove first value of array " + Arrays.toString(fin));
 
-        //fin = removeIndex(scores, 2);
-        System.out.println("remove a value from an index (2) of the array " + Arrays.toString(fin));
+        first_arr =  new int[]{5, 6, 7 ,8};
+
+        fin = removeIndex(first_arr, 1);
+        System.out.println("remove a value from an index (1) of the array " + Arrays.toString(fin));
 
 
     }
 
 
     public static int[] makeCopyOf(int[] arr){
-        //return Arrays.copyOf(arr, arr.length);
-        int[] first_arr = {5, 6, 7 ,8}
-        int[] second_arr = makeCopyOf(first_arr)
-        //second_arr should also have 5,6, 7, 8
-        //furthermore,
-        second_arr[1] = 12; //Should not change first_arr
-        print(first_arr) -> {5, 6, 7 ,8}
-        print(second_arr) -> {5, 12, 7 ,8}
-    
+        for(int i =0; i< arr.length; i++){
+            arr[1] = 12;
+        }
+        return arr;    
     }
 
+    public static int[] addValue(int[] arr, int val, int pos){
+        val = -9;
+        int[] temp = new int[arr.length+1];
+        for(int i =0; i< pos; i++){
+            temp[i] = arr[i];
+        }
+        temp[pos] = val;
+        temp[4]=arr[3];
+        for(int i= pos+1; i<arr.length; i++){
+            temp[i]=arr[i-1];
+        }
 
-    public static int[] addValue(int[] arr, int val){
-        int[] arr = { 5, 6, 7 ,8 }
-        arr = addValueAt( arr, -9, 3)
-        // { 5, 6, 7, -9, 8}
-
-/*        int[] tempArr = new int[arr.length+ 1];
-        System.arraycopy(arr, 0, tempArr, 0, arr.length);
-        
-        for(int i=0; i < arr.length; i++)
-            tempArr[i] = arr[i];
-        tempArr[tempArr.length-1] = val;
-        return tempArr;*/
+        arr = temp;
+        return arr;
     }
 
     public static int[] removeLast(int[] arr){ 
-
-        int[] arr = { 5, 6, 7 ,8 }
-        arr = removeLast( arr)
-        // arr should look like { 5, 6, 7}
-
-
-        /*int[] tempArr = new int[arr.length-1];
-        System.arraycopy(arr, 0, tempArr, 0, arr.length-1);
-
-        for (int i=1; i < arr.length; i++){
-            tempArr[i-1] = arr[i];
-        }       
-        return tempArr;*/
+        int[] temp = new int[arr.length-1];
+        for(int i=0; i<arr.length-1; i++){
+            temp[i]=arr[i];
+        }
+        arr = temp;
+        return arr;
     }
 
-    public static int[] removeIndex(int[] arr, int index){
-        int[] arr = { 5, 6, 7 ,8 }
-        arr = removeIndex(arr, 1)
-        //arr should look like { 5, 7, 8 }
-        
+    public static int[] removeIndex(int[] arr, int pos){
+        int[] temp = new int[arr.length-1];
+        for (int i=0; i < arr.length-1; i++){
+            temp[i]=arr[i];
+        }
+        temp[2] = arr[3];
+        for(int i=pos+1; i<arr.length-1; i++){
+            temp[pos]=arr[i];
+        }
+        arr = temp;
+        return arr;
     }
 }
 
